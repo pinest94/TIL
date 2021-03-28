f73e062caf9124a0d2f9348ca1944c5798b4c561
김한솔 <doingnow94@gmail.com>
2021-03-28 21:10:49 +0900

    [210328] Spring Data JPA - save, page, QBE관련 학습

--- JPA/src/test/java/com/pinest94/jpa/bookmanager/domain/UserRepositoryTest.java
+++ JPA/src/test/java/com/pinest94/jpa/bookmanager/domain/UserRepositoryTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -16,7 +18,7 @@ class UserRepositoryTest {
 
     @Test
     // @Transactional // 세션을 해당 함수에 유지시켜줌
-    void createTest() {
+    void crudTest() {
 
         // 1. create
         User user1 = User.builder().name("jack").email("jack@pinest.com").build();
@@ -48,7 +50,56 @@ class UserRepositoryTest {
 
         // User user = userRepository.getOne(1L);
 
-        User user = userRepository.findById(1L).orElse(null);
-        System.out.println(user);
+        // User user = userRepository.findById(1L).orElse(null);
+        // System.out.println(user);
+
+        // 4. delete
+        // delete메서드에 파라미터로 null값이 들어갈 수 없다.(중요)
+        // userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));
+        // userRepository.deleteById(1L);
+        
+        // delete와 deleteInBatch의 차이점
+        // delete의 경우 select를 통해 해당 엔티티가 있는 지 한번 더 확인한다.
+        // 두번 째는 delete할 개수만큼 for-loop를 이용하여 삭제한다 (ex. 1000만개 row삭제 시 1000만회 쿼리실행)
+        // deleteInBatch는 엔티티가 있는지 없는지 확인하지 않고 여러개의 row도 하나의 쿼리로 삭제하므로 훨씬 성능이 좋다.
+        // userRepository.deleteAll(userRepository.findAllById(Lists.newArrayList(1L, 3L)));
+        userRepository.deleteAllInBatch();
+        userRepository.findAll().forEach(System.out::println);
+    }
+
+    @Test
+    public void updateTest() {
+        userRepository.save(User.builder().name("kris").email("kris@gmail.com").build());
+
+        User user = userRepository.findById(1L).orElseThrow(RuntimeException::new);
+        user.setEmail("kris@naver.com");
+        userRepository.save(user);
+    }
+
+    @Test
+    public void pageTest() {
+        Page<User> users = userRepository.findAll(PageRequest.of(1, 5));
+
+        System.out.println("page : " + users);
+        System.out.println("totalElements : " + users.getTotalElements());
+        System.out.println("totalPage : " + users.getTotalPages());
+        System.out.println("numberOfElements : " + users.getNumberOfElements());
+        System.out.println("sort : " + users.getSort());
+        System.out.println("size : " + users.getSize());
+
+        users.getContent().forEach(System.out::print);
+    }
+
+    // QBE : Query By Example
+    @Test
+    public void QBETest() {
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                .withIgnorePaths("name")
+                .withMatcher("email", endsWith());
+
+        Example<User> example = Example.of(new User("ma", "pinest.com"), exampleMatcher);
+
+        userRepository.findAll(example).forEach(System.out::println);
+
     }
 }
